0fa140e73b20fa364ce448e550cf8872528727a6
cg <c.g.mail@gmx.net>
2022-07-13 21:00:40 +0200

    added diagnostic methods..

--- BD16939.cpp
+++ BD16939.cpp
@@ -12,6 +12,188 @@
 
 using namespace BD16939;
 
+bool Device::getreg(regbit reg)
+{
+    switch (reg)
+    {
+        // ovps,       // Over Voltage Protection Status
+    case ovps:
+        return (uint8_t)bitRead(reg_out3, OVPS);
+        break;
+        // uvlos,      // Under-Voltage-Lock-Out Status
+    case uvlos:
+        return (uint8_t)bitRead(reg_out3, UVLOS);
+        break;
+        // tsds,       // Thermal Shutdown Status
+    case tsds:
+        return (uint8_t)bitRead(reg_out2, TSDS);
+        break;
+        // tws,        // Thermal Warning Status
+    case tws:
+        return (uint8_t)bitRead(reg_out2, TWS);
+        break;
+        // hss1,       // Status High Side 1 (OUT1)
+    case hss1:
+        return (uint8_t)bitRead(reg_out1, HSS1);
+        break;
+        // hss2,       // Status High Side 2 (OUT2)
+    case hss2:
+        return (uint8_t)bitRead(reg_out1, HSS2);
+        break;
+        // hss3,       // Status High Side 3 (OUT3)
+    case hss3:
+        return (uint8_t)bitRead(reg_out1, HSS3);
+        break;
+        // hss4,       // Status High Side 4 (OUT4)
+    case hss4:
+        return (uint8_t)bitRead(reg_out1, HSS4);
+        break;
+        // hss5,       // Status High Side 5 (OUT5)
+    case hss5:
+        return (uint8_t)bitRead(reg_out3, HSS5);
+        break;
+        // hss6,       // Status High Side 6 (OUT6)
+    case hss6:
+        return (uint8_t)bitRead(reg_out3, HSS6);
+        break;
+        // lss1,       // Status Low Side 1 (OUT1)
+    case lss1:
+        return (uint8_t)bitRead(reg_out1, LSS1);
+        break;
+        // lss2,       // Status Low Side 2 (OUT2)
+    case lss2:
+        return (uint8_t)bitRead(reg_out1, LSS2);
+        break;
+        // lss3,       // Status Low Side 3 (OUT3)
+    case lss3:
+        return (uint8_t)bitRead(reg_out1, LSS3);
+        break;
+        // lss4,       // Status Low Side 4 (OUT4)
+    case lss4:
+        return (uint8_t)bitRead(reg_out1, LSS4);
+        break;
+        // lss5,       // Status Low Side 5 (OUT5)
+    case lss5:
+        return (uint8_t)bitRead(reg_out3, LSS5);
+        break;
+        // lss6,       // Status Low Side 6 (OUT6)
+    case lss6:
+        return (uint8_t)bitRead(reg_out3, LSS6);
+        break;
+        // ocps,       // Over Current Protection Status
+    case ocps:
+        return (uint8_t)bitRead(reg_out3, OCPS);
+        break;
+        // ocph1,      // Over Current Protection High Side Status OUT1
+    case ocph1:
+        return (uint8_t)bitRead(reg_out2, OCPH1);
+        break;
+        // ocph2,      // Over Current Protection High Side Status OUT2
+    case ocph2:
+        return (uint8_t)bitRead(reg_out2, OCPH2);
+        break;
+        // ocph3,      // Over Current Protection High Side Status OUT3
+    case ocph3:
+        return (uint8_t)bitRead(reg_out2, OCPH3);
+        break;
+        // ocph4,      // Over Current Protection High Side Status OUT4
+    case ocph4:
+        return (uint8_t)bitRead(reg_out2, OCPH4);
+        break;
+        // ocph5,      // Over Current Protection High Side Status OUT5
+    case ocph5:
+        return (uint8_t)bitRead(reg_out4, OCPH5);
+        break;
+        // ocph6,      // Over Current Protection High Side Status OUT6
+    case ocph6:
+        return (uint8_t)bitRead(reg_out4, OCPH6);
+        break;
+        // ocpl1,      // Over Current Protection Low Side Status OUT1
+    case ocpl1:
+        return (uint8_t)bitRead(reg_out2, OCPL1);
+        break;
+        // ocpl2,      // Over Current Protection Low Side Status OUT2
+    case ocpl2:
+        return (uint8_t)bitRead(reg_out2, OCPL2);
+        break;
+        // ocpl3,      // Over Current Protection Low Side Status OUT3
+    case ocpl3:
+        return (uint8_t)bitRead(reg_out2, OCPL3);
+        break;
+        // ocpl4,      // Over Current Protection Low Side Status OUT4
+    case ocpl4:
+        return (uint8_t)bitRead(reg_out2, OCPL4);
+        break;
+        // ocpl5,      // Over Current Protection Low Side Status OUT5
+    case ocpl5:
+        return (uint8_t)bitRead(reg_out4, OCPL5);
+        break;
+        // ocpl6,      // Over Current Protection Low Side Status OUT6
+    case ocpl6:
+        return (uint8_t)bitRead(reg_out4, OCPL6);
+        break;
+        // underloads, // Under Loads Status
+    case underloads:
+        return (uint8_t)bitRead(reg_out3, UNDERLOADS);
+        break;
+        // underload1, // Under Load Status OUT1
+    case underload1:
+        return (uint8_t)bitRead(reg_out2, UNDERLOAD1);
+        break;
+        // underload2, // Under Load Status OUT2
+    case underload2:
+        return (uint8_t)bitRead(reg_out2, UNDERLOAD2);
+        break;
+        // underload3, // Under Load Status OUT3
+    case underload3:
+        return (uint8_t)bitRead(reg_out2, UNDERLOAD3);
+        break;
+        // underload4, // Under Load Status OUT4
+    case underload4:
+        return (uint8_t)bitRead(reg_out2, UNDERLOAD4);
+        break;
+        // underload5, // Under Load Status OUT5
+    case underload5:
+        return (uint8_t)bitRead(reg_out4, UNDERLOAD5);
+        break;
+        // underload6, // Under Load Status OUT6
+    case underload6:
+        return (uint8_t)bitRead(reg_out4, UNDERLOAD6);
+        break;
+    }
+    state = ERROR_READ;
+    return false;
+}
+
+uint16_t Device::getregint(regname rn)
+{
+    switch (rn)
+    {
+    case regin1:
+        return reg_in1;
+        break;
+    case regin2:
+        return reg_in2;
+        break;
+    case regout1:
+        return reg_out1;
+        break;
+    case regout2:
+        return reg_out2;
+        break;
+    case regout3:
+        return reg_out3;
+        break;
+    case regout4:
+        return reg_out4;
+        break;
+
+    default:
+        return 0;
+        break;
+    }
+}
+
 void Device::updateregs()
 {
     spi->beginTransaction(spisettings);
@@ -19,22 +201,21 @@ void Device::updateregs()
     digitalWrite(sspin, LOW);
     reg_out1 = spi->transfer16(reg_in1); // Write input-register 1 and read output register A0
     digitalWrite(sspin, HIGH);
-    delayMicroseconds(10);
+    delayMicroseconds(20);
     digitalWrite(sspin, LOW);
     reg_out3 = spi->transfer16(reg_in2); // Write input-register 2 and read output register B0
     digitalWrite(sspin, HIGH);
-    delayMicroseconds(10);
+    delayMicroseconds(20);
     digitalWrite(sspin, LOW);
     reg_out2 = spi->transfer16(OUTPUT_REGA1); // Write nothing and read output register A1
     digitalWrite(sspin, HIGH);
-    delayMicroseconds(10);
+    delayMicroseconds(20);
     digitalWrite(sspin, LOW);
     reg_out4 = spi->transfer16(OUTPUT_REGB1); // Write nothing and read output register B1
     digitalWrite(sspin, HIGH);
+    delayMicroseconds(20);
 
     spi->endTransaction();
-
-    printregs();
 }
 
 void Device::printstate()
@@ -56,26 +237,11 @@ void Device::printstate()
         break;
     case ERROR_WRITE:
         serial->println("ERROR_WRITE");
-        break;    
+        break;
     case ERROR_READ:
         serial->println("ERROR_READ");
-        break;   
+        break;
     }
-}
-void Device::printregs()
-{
-    serial->print(" in1: ");
-    serial->print(reg_in1);
-    serial->print(" in2: ");
-    serial->println(reg_in2);
-    serial->print(" out1: ");
-    serial->print(reg_out1);
-    serial->print(" out2: ");
-    serial->print(reg_out2);
-    serial->print(" out3: ");
-    serial->print(reg_out3);
-    serial->print(" out4: ");
-    serial->println(reg_out4);
 }
 
 void Device::reset()
@@ -89,14 +255,10 @@ void Device::reset()
 
 bool Device::begin(SPIClass *extspi, uint8_t ss)
 {
-    spi = extspi;
-    sspin = ss;
-    spi->setHwCs(false);
-    pinMode(sspin, OUTPUT);
-
     // ---------- prepare input register A --------------
     reg_in1 = INPUT_REGA; // set base address
-    // bitWrite(reg_in1, SSR, 1);       // Reset all faults
+    bitWrite(reg_in2, RD_AD, 0);
+    bitWrite(reg_in1, SSR, 1);       // Reset all faults
     bitWrite(reg_in1, UNDERLOAD, 1); // Underload detection off
     bitWrite(reg_in1, TSDSTH, 1);    // Thermal Shutdown Self Recovery on
     bitWrite(reg_in1, PSSTH, 1);     // Over-Voltage and Under-Voltage Self Recovery on
@@ -104,9 +266,16 @@ bool Device::begin(SPIClass *extspi, uint8_t ss)
     // ---------- prepare input register B --------------
     reg_in2 = INPUT_REGB;
     bitWrite(reg_in2, RD_AD, 1);
-    // bitWrite(reg_in2, SSR, 1); // Reset all faults
+    bitWrite(reg_in2, OVPSEL, 0); // set Overvoltage-Threshold to 36V
+    bitWrite(reg_in2, SSR, 1);    // Reset all faults
 
-    reset();
+    spi = extspi;
+    sspin = ss;
+    spi->setHwCs(false);
+    pinMode(sspin, OUTPUT);
+
+    updateregs();
+    // reset();
 
     serial->print("Driver init..");
     if (reg_out1 | reg_out2 | reg_out3 | reg_out4)
--- BD16939.h
+++ BD16939.h
@@ -41,7 +41,7 @@ motA (hb1+hb2) , motB (hb3+hb4) and motC (hb5 + hb6).
 
 #define BD16939_LIB_VERSION (F("0.1.0"))
 
-extern HardwareSerial* serial;
+extern HardwareSerial *serial;
 
 // STATUS/ERROR CODES
 #define OK 1
@@ -55,7 +55,7 @@ namespace BD16939
 {
   enum halfbridge
   {
-    hb1=1,
+    hb1 = 1,
     hb2,
     hb3,
     hb4,
@@ -65,7 +65,7 @@ namespace BD16939
 
   enum fullbridge
   {
-    motA=1,
+    motA = 1,
     motB,
     motC,
     all,
@@ -84,6 +84,82 @@ namespace BD16939
     right,
   };
 
+  enum regbit
+  {
+    // ****************************************************************
+    // ********************** WRITE-REGISTERS *************************
+    // ****************************************************************
+    we,        // Write-Enable
+    wr_ad,     // REG A/B
+    rd_ad,     // REG-PAGE 0/1
+    ssr,       // Status Reset Register (This bit will self-clear)
+    underload, // Under Loads Register Mode (OUT1 to OUT6)
+    tsdsth,    // Thermal-Shut-Down Register Mode
+    pssth,     // Over-Voltage-Protection/Under-Voltage-Detection
+    hsc1,      // High Side 1 Set (OUT1)
+    hsc2,      // High Side 2 Set (OUT2)
+    hsc3,      // High Side 3 Set (OUT3)
+    hsc4,      // High Side 4 Set (OUT4)
+    hsc5,      // High Side 5 Set (OUT5)
+    hsc6,      // High Side 6 Set (OUT6)
+    lsc1,      // Low Side 1 Set (OUT1)
+    lsc2,      // Low Side 2 Set (OUT2)
+    lsc3,      // Low Side 3 Set (OUT3)
+    lsc4,      // Low Side 4 Set (OUT4)
+    lsc5,      // Low Side 5 Set (OUT5)
+    lsc6,      // Low Side 6 Set (OUT6)
+
+    // ****************************************************************
+    // ********************** READ-REGISTERS **************************
+    // ****************************************************************
+    ovps,       // Over Voltage Protection Status
+    uvlos,      // Under-Voltage-Lock-Out Status
+    tsds,       // Thermal Shutdown Status
+    tws,        // Thermal Warning Status
+    hss1,       // Status High Side 1 (OUT1)
+    hss2,       // Status High Side 2 (OUT2)
+    hss3,       // Status High Side 3 (OUT3)
+    hss4,       // Status High Side 4 (OUT4)
+    hss5,       // Status High Side 5 (OUT5)
+    hss6,       // Status High Side 6 (OUT6)
+    lss1,       // Status Low Side 1 (OUT1)
+    lss2,       // Status Low Side 2 (OUT2)
+    lss3,       // Status Low Side 3 (OUT3)
+    lss4,       // Status Low Side 4 (OUT4)
+    lss5,       // Status Low Side 5 (OUT5)
+    lss6,       // Status Low Side 6 (OUT6)
+    ocps,       // Over Current Protection Status
+    ocph1,      // Over Current Protection High Side Status OUT1
+    ocph2,      // Over Current Protection High Side Status OUT2
+    ocph3,      // Over Current Protection High Side Status OUT3
+    ocph4,      // Over Current Protection High Side Status OUT4
+    ocph5,      // Over Current Protection High Side Status OUT5
+    ocph6,      // Over Current Protection High Side Status OUT6
+    ocpl1,      // Over Current Protection Low Side Status OUT1
+    ocpl2,      // Over Current Protection Low Side Status OUT2
+    ocpl3,      // Over Current Protection Low Side Status OUT3
+    ocpl4,      // Over Current Protection Low Side Status OUT4
+    ocpl5,      // Over Current Protection Low Side Status OUT5
+    ocpl6,      // Over Current Protection Low Side Status OUT6
+    underloads, // Under Loads Status
+    underload1, // Under Load Status OUT1
+    underload2, // Under Load Status OUT2
+    underload3, // Under Load Status OUT3
+    underload4, // Under Load Status OUT4
+    underload5, // Under Load Status OUT5
+    underload6, // Under Load Status OUT6
+  };
+
+  enum regname
+  {
+    regin1,
+    regin2,
+    regout1,
+    regout2,
+    regout3, 
+    regout4,
+  };
+
   class Device
   {
   private:
@@ -98,7 +174,6 @@ namespace BD16939
     uint16_t reg_out3;
     uint16_t reg_out4;
 
-    void updateregs(); // Sends the input-registers and recieves all four output-registers
     
 
   public:
@@ -109,16 +184,18 @@ namespace BD16939
     {
     }
 
+    void updateregs(); // Sends the input-registers and recieves all four output-registers
     bool begin(SPIClass * /*pSPIClass*/, uint8_t /*SS*/, uint32_t /*SPI-Frequency*/);
     bool begin(SPIClass * /*pSPIClass*/, uint8_t /*SS*/);
 
     uint16_t send(uint16_t); // ..just for debugging purposes..
     int8_t setbridge(halfbridge /*hb-number(1->6)*/, switchstate);
     int8_t setmotor(fullbridge /*Motor*/, switchstate);
-    int8_t getstate();
-    void printstate(); // print out driver state..
-    void printregs(); // print out all input and output register..
-    void reset(); // reset driver
+    uint16_t getregint(regname); // get specified Register content as integer value
+    bool getreg(regbit); // get specified Register-bit as bool value
+    void printstate();    // print out driver state..
+    void printregs();     // print out all input and output register..
+    void reset();         // reset driver
   };
 };
 
